5c7159e50c93c908db00a54f8a243cb90e5e16c4
abrahamx97 <abrahamx97@gmail.com>
2018-03-03 20:16:36 -0600

    Operacion C/CRUD completada

--- Inventario2.0/Detalle_Inventario.Designer.cs
+++ Inventario2.0/Detalle_Inventario.Designer.cs
@@ -28,11 +28,303 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewDetalle = new System.Windows.Forms.DataGridView();
+            this.id_articulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.articulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtEncargado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtObservaciones = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboEdificios = new System.Windows.Forms.ComboBox();
+            this.comboSalones = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboArticulos = new System.Windows.Forms.ComboBox();
+            this.cantidadArticulos = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnGuardarArticulo = new System.Windows.Forms.Button();
+            this.btnEliminarArticulo = new System.Windows.Forms.Button();
+            this.btnCancelarInventario = new System.Windows.Forms.Button();
+            this.btnGuardarInvetario = new System.Windows.Forms.Button();
+            this.btnCancelarArticulo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDetalle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cantidadArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewDetalle
+            // 
+            this.dataGridViewDetalle.AllowUserToAddRows = false;
+            this.dataGridViewDetalle.AllowUserToDeleteRows = false;
+            this.dataGridViewDetalle.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDetalle.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_articulo,
+            this.articulo,
+            this.cantidad});
+            this.dataGridViewDetalle.GridColor = System.Drawing.Color.White;
+            this.dataGridViewDetalle.Location = new System.Drawing.Point(13, 32);
+            this.dataGridViewDetalle.MultiSelect = false;
+            this.dataGridViewDetalle.Name = "dataGridViewDetalle";
+            this.dataGridViewDetalle.ReadOnly = true;
+            this.dataGridViewDetalle.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewDetalle.Size = new System.Drawing.Size(498, 386);
+            this.dataGridViewDetalle.TabIndex = 0;
+            this.dataGridViewDetalle.DoubleClick += new System.EventHandler(this.dataGridViewDetalle_DoubleClick);
+            // 
+            // id_articulo
+            // 
+            this.id_articulo.HeaderText = "id_articulo";
+            this.id_articulo.Name = "id_articulo";
+            this.id_articulo.ReadOnly = true;
+            this.id_articulo.Visible = false;
+            // 
+            // articulo
+            // 
+            this.articulo.DataPropertyName = "articulo";
+            this.articulo.HeaderText = "Articulo";
+            this.articulo.MinimumWidth = 200;
+            this.articulo.Name = "articulo";
+            this.articulo.ReadOnly = true;
+            this.articulo.Width = 350;
+            // 
+            // cantidad
+            // 
+            this.cantidad.DataPropertyName = "cantidad";
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.MinimumWidth = 70;
+            this.cantidad.Name = "cantidad";
+            this.cantidad.ReadOnly = true;
+            // 
+            // txtEncargado
+            // 
+            this.txtEncargado.Location = new System.Drawing.Point(531, 486);
+            this.txtEncargado.Name = "txtEncargado";
+            this.txtEncargado.Size = new System.Drawing.Size(274, 20);
+            this.txtEncargado.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 459);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Encargado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Observaciones";
+            // 
+            // txtObservaciones
+            // 
+            this.txtObservaciones.Location = new System.Drawing.Point(13, 486);
+            this.txtObservaciones.Multiline = true;
+            this.txtObservaciones.Name = "txtObservaciones";
+            this.txtObservaciones.Size = new System.Drawing.Size(498, 160);
+            this.txtObservaciones.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(531, 530);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Edificio";
+            // 
+            // comboEdificios
+            // 
+            this.comboEdificios.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboEdificios.FormattingEnabled = true;
+            this.comboEdificios.Location = new System.Drawing.Point(531, 557);
+            this.comboEdificios.Name = "comboEdificios";
+            this.comboEdificios.Size = new System.Drawing.Size(274, 21);
+            this.comboEdificios.TabIndex = 6;
+            this.comboEdificios.SelectedIndexChanged += new System.EventHandler(this.comboEdificios_SelectedIndexChanged);
+            // 
+            // comboSalones
+            // 
+            this.comboSalones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboSalones.FormattingEnabled = true;
+            this.comboSalones.Location = new System.Drawing.Point(531, 625);
+            this.comboSalones.Name = "comboSalones";
+            this.comboSalones.Size = new System.Drawing.Size(274, 21);
+            this.comboSalones.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(531, 598);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Salón";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(531, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Articulo";
+            // 
+            // comboArticulos
+            // 
+            this.comboArticulos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboArticulos.FormattingEnabled = true;
+            this.comboArticulos.Location = new System.Drawing.Point(534, 76);
+            this.comboArticulos.Name = "comboArticulos";
+            this.comboArticulos.Size = new System.Drawing.Size(271, 21);
+            this.comboArticulos.TabIndex = 10;
+            // 
+            // cantidadArticulos
+            // 
+            this.cantidadArticulos.Location = new System.Drawing.Point(534, 161);
+            this.cantidadArticulos.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.cantidadArticulos.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.cantidadArticulos.Name = "cantidadArticulos";
+            this.cantidadArticulos.Size = new System.Drawing.Size(271, 20);
+            this.cantidadArticulos.TabIndex = 11;
+            this.cantidadArticulos.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(531, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Cantidad";
+            // 
+            // btnGuardarArticulo
+            // 
+            this.btnGuardarArticulo.Location = new System.Drawing.Point(534, 221);
+            this.btnGuardarArticulo.Name = "btnGuardarArticulo";
+            this.btnGuardarArticulo.Size = new System.Drawing.Size(122, 23);
+            this.btnGuardarArticulo.TabIndex = 13;
+            this.btnGuardarArticulo.Text = "Guardar";
+            this.btnGuardarArticulo.UseVisualStyleBackColor = true;
+            this.btnGuardarArticulo.Click += new System.EventHandler(this.btnGuardarArticulo_Click);
+            // 
+            // btnEliminarArticulo
+            // 
+            this.btnEliminarArticulo.Enabled = false;
+            this.btnEliminarArticulo.Location = new System.Drawing.Point(683, 221);
+            this.btnEliminarArticulo.Name = "btnEliminarArticulo";
+            this.btnEliminarArticulo.Size = new System.Drawing.Size(122, 23);
+            this.btnEliminarArticulo.TabIndex = 14;
+            this.btnEliminarArticulo.Text = "Eliminar";
+            this.btnEliminarArticulo.UseVisualStyleBackColor = true;
+            this.btnEliminarArticulo.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnCancelarInventario
+            // 
+            this.btnCancelarInventario.Location = new System.Drawing.Point(683, 683);
+            this.btnCancelarInventario.Name = "btnCancelarInventario";
+            this.btnCancelarInventario.Size = new System.Drawing.Size(122, 23);
+            this.btnCancelarInventario.TabIndex = 16;
+            this.btnCancelarInventario.Text = "Cancelar";
+            this.btnCancelarInventario.UseVisualStyleBackColor = true;
+            this.btnCancelarInventario.Click += new System.EventHandler(this.btnCancelarInventario_Click);
+            // 
+            // btnGuardarInvetario
+            // 
+            this.btnGuardarInvetario.Location = new System.Drawing.Point(534, 683);
+            this.btnGuardarInvetario.Name = "btnGuardarInvetario";
+            this.btnGuardarInvetario.Size = new System.Drawing.Size(122, 23);
+            this.btnGuardarInvetario.TabIndex = 15;
+            this.btnGuardarInvetario.Text = "Guardar";
+            this.btnGuardarInvetario.UseVisualStyleBackColor = true;
+            this.btnGuardarInvetario.Click += new System.EventHandler(this.btnGuardarInvetario_Click);
+            // 
+            // btnCancelarArticulo
+            // 
+            this.btnCancelarArticulo.Enabled = false;
+            this.btnCancelarArticulo.Location = new System.Drawing.Point(534, 264);
+            this.btnCancelarArticulo.Name = "btnCancelarArticulo";
+            this.btnCancelarArticulo.Size = new System.Drawing.Size(271, 23);
+            this.btnCancelarArticulo.TabIndex = 17;
+            this.btnCancelarArticulo.Text = "Cancelar";
+            this.btnCancelarArticulo.UseVisualStyleBackColor = true;
+            this.btnCancelarArticulo.Click += new System.EventHandler(this.btnCancelarArticulo_Click);
+            // 
+            // Detalle_Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(817, 733);
+            this.Controls.Add(this.btnCancelarArticulo);
+            this.Controls.Add(this.btnCancelarInventario);
+            this.Controls.Add(this.btnGuardarInvetario);
+            this.Controls.Add(this.btnEliminarArticulo);
+            this.Controls.Add(this.btnGuardarArticulo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cantidadArticulos);
+            this.Controls.Add(this.comboArticulos);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboSalones);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboEdificios);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtObservaciones);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEncargado);
+            this.Controls.Add(this.dataGridViewDetalle);
+            this.Name = "Detalle_Inventario";
             this.Text = "Detalle_Inventario";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Detalle_Inventario_FormClosing);
+            this.Load += new System.EventHandler(this.Detalle_Inventario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDetalle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cantidadArticulos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewDetalle;
+        private System.Windows.Forms.TextBox txtEncargado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtObservaciones;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboEdificios;
+        private System.Windows.Forms.ComboBox comboSalones;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboArticulos;
+        private System.Windows.Forms.NumericUpDown cantidadArticulos;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnGuardarArticulo;
+        private System.Windows.Forms.Button btnEliminarArticulo;
+        private System.Windows.Forms.Button btnCancelarInventario;
+        private System.Windows.Forms.Button btnGuardarInvetario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_articulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn articulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad;
+        private System.Windows.Forms.Button btnCancelarArticulo;
     }
 }
--- Inventario2.0/Detalle_Inventario.cs
+++ Inventario2.0/Detalle_Inventario.cs
@@ -1,20 +1,231 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Collections;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Inventario2._0
 {
     public partial class Detalle_Inventario : Form
     {
+        private ArrayList idsArticulo = new ArrayList();
+        private bool editing = false;
+        private int id_articulo_selected = -1;
+        private int id_inventario_selected = -1;
+
+        private DataGridViewRow selectedRow = null;
+
+        private Database db = null;
+        private MySqlDataAdapter mySqlDataAdapter;
+
         public Detalle_Inventario()
         {
             InitializeComponent();
+        }
+
+        public Detalle_Inventario(int id_inventario_selected)
+        {
+            InitializeComponent();
+            this.id_inventario_selected = id_inventario_selected;
+            db = new Database();
+
+
+            if (db.openConnection())
+            {
+                this.fillTable();
+            }
+            
+        }
+
+        private void Detalle_Inventario_Load(object sender, EventArgs e)
+        {
+            db = new Database();
+
+            if (db.openConnection())
+            {
+                if (this.id_inventario_selected != -1)
+                {
+                    this.fillTable();
+                }
+                this.fillComboArticulos();
+                this.fillComboEdificios();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (this.selectedRow != null)
+            {
+                this.dataGridViewDetalle.Rows.Remove(this.selectedRow);
+                this.idsArticulo.Remove(Int32.Parse(this.selectedRow.Cells[0].Value.ToString()));
+                this.resetFields();
+            }
+        }
+
+        private void comboEdificios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.fillComboSalones();
+        }
+
+        private void btnGuardarArticulo_Click(object sender, EventArgs e)
+        {
+
+            if (this.editing)
+            {
+                this.dataGridViewDetalle.Rows.Remove(selectedRow);
+            }
+            else
+            {
+                if (this.comboArticulos.Items.Count <= 0) return;
+                if (this.idsArticulo.Contains(Int32.Parse(this.comboArticulos.SelectedValue.ToString()))) return;
+            }
+
+            this.dataGridViewDetalle.Rows.Add(new Object[] { this.comboArticulos.SelectedValue, this.comboArticulos.GetItemText(this.comboArticulos.SelectedItem), this.cantidadArticulos.Value });
+
+            if(!editing) this.idsArticulo.Add(Int32.Parse(this.comboArticulos.SelectedValue.ToString()));
+            this.resetFields();
+        }
+
+        private void dataGridViewDetalle_DoubleClick(object sender, EventArgs e)
+        {
+            this.getRowFields();
+            if (this.selectedRow != null)
+            {
+                this.btnGuardarArticulo.Text = "Actualizar";
+                this.editing = true;
+                this.btnCancelarArticulo.Enabled = true;
+                this.btnEliminarArticulo.Enabled = true;
+            }
+        }
+
+        private void btnCancelarArticulo_Click(object sender, EventArgs e)
+        {
+            this.resetFields();
+        }
+
+        //class helpers
+        private void fillTable()
+        {
+            this.mySqlDataAdapter = db.getMySqlDataAdapter("select di.id_articulo, di.cantidad, a.nombre as articulo from detalle_inventario as di inner join articulos as a on di.id_articulo = a.id_articulo where di.id_inventario = " + this.id_inventario_selected );
+            DataSet dataSet = new DataSet();
+            mySqlDataAdapter.Fill(dataSet);
+            this.dataGridViewDetalle.DataSource = dataSet.Tables[0];    //rellena la tabla del inventario seleccionado
+
+
+            //añade cada id_articulo existente a un array lista para que no se pueda agregar dos veces
+            foreach (DataGridViewRow row in this.dataGridViewDetalle.Rows)
+            {
+                this.idsArticulo.Add(Int32.Parse(row.Cells[0].Value.ToString()));
+            }
+        }
+
+        private void fillComboArticulos()
+        {
+            mySqlDataAdapter = db.getMySqlDataAdapter("select id_articulo, nombre from articulos");
+            DataSet dataset = new DataSet();
+            mySqlDataAdapter.Fill(dataset);
+
+            comboArticulos.ValueMember = "id_articulo";
+            comboArticulos.DisplayMember = "nombre";
+            comboArticulos.DataSource = dataset.Tables[0].DefaultView;
+        }
+
+        private void fillComboEdificios()
+        {
+            mySqlDataAdapter = db.getMySqlDataAdapter("select id_edificio, nombre from edificios");
+            DataSet dataset = new DataSet();
+            mySqlDataAdapter.Fill(dataset);
+
+            comboEdificios.ValueMember = "id_edificio";
+            comboEdificios.DisplayMember = "nombre";
+            comboEdificios.DataSource = dataset.Tables[0].DefaultView;
+        }
+
+        private void fillComboSalones()
+        {
+            mySqlDataAdapter = db.getMySqlDataAdapter("select id_salon, nombre from salones where id_edificio = "+ this.comboEdificios.SelectedValue);
+            DataSet dataset = new DataSet();
+            mySqlDataAdapter.Fill(dataset);
+
+            comboSalones.ValueMember = "id_salon";
+            comboSalones.DisplayMember = "nombre";
+            comboSalones.DataSource = dataset.Tables[0].DefaultView;
+        }
+
+        private void getRowFields()
+        {
+            //extrae los datos de la(s) fila seleccionada 
+            DataGridViewSelectedRowCollection rows = this.dataGridViewDetalle.SelectedRows;
+            foreach (DataGridViewRow row in rows)
+            {
+                this.id_articulo_selected = Int32.Parse(row.Cells[0].Value.ToString()); //id_articulo
+                this.comboArticulos.SelectedValue = row.Cells[0].Value.ToString();
+                this.cantidadArticulos.Value = Int32.Parse(row.Cells[2].Value.ToString());  //cantidad
+
+                this.selectedRow = row;     //permitira borrar la fila 
+            }
+        }
+
+        private void resetFields()
+        {
+            this.btnCancelarArticulo.Enabled = false;
+            this.btnEliminarArticulo.Enabled = false;
+            this.editing = false;
+            this.btnGuardarArticulo.Text = "Guardar";
+            this.comboArticulos.SelectedIndex = 0;
+            this.cantidadArticulos.Value = 1;
+            this.selectedRow = null;
+        }
+
+        private void btnGuardarInvetario_Click(object sender, EventArgs e)
+        {
+            if(this.txtEncargado.Text.Length==0)
+            {
+                this.txtEncargado.Focus();
+                return;
+            }
+
+            if (this.dataGridViewDetalle.RowCount <= 0) return;
+
+            db = new Database();
+            if (this.db.openConnection())
+            {
+                int rowsAffected = this.db.executeNonQuery("insert into inventarios(id_salon, encargado, observaciones, fecha_creacion, fecha_actualizado) values("+this.comboSalones.SelectedValue.ToString()+", '"+this.txtEncargado.Text+"' , '"+this.txtObservaciones.Text+"', now() , now())");
+                if (rowsAffected > 0)
+                {
+                    string values = "";
+
+                    foreach (DataGridViewRow row in this.dataGridViewDetalle.SelectedRows)
+                    {
+                        values += "(@id_inventario, " + row.Cells[0].Value.ToString() + ", " + row.Cells[2].Value.ToString() + "),";
+                    }
+
+                    int save = this.db.executeNonQuery("set @id_inventario = (select id_inventario from inventarios order by id_inventario desc limit 1); insert into detalle_inventario(id_inventario, id_articulo, cantidad) values " + values.Substring(0, values.Length-1)+";");
+
+                    if (save > 0)
+                    {
+                        System.Console.WriteLine("Saved");
+                    }
+                }
+            }
+        }
+
+        private void btnCancelarInventario_Click(object sender, EventArgs e)
+        {
+            if (this.db != null)
+            {
+                this.db.closeConnection();
+            }
+            this.Hide();
+        }
+
+        private void Detalle_Inventario_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.db != null)
+            {
+                this.db.closeConnection();
+            }
+            this.Hide();
         }
     }
 }
